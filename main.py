f85c5d12222571cab84be16f22fb363b7634289f
Rokon Ron <rokon.raz@gmail.com>
2023-04-29 08:46:51 +0600

    Open a file and modifi a letter.

---
+++ main.py
@@ -0,0 +1,28 @@
+# POSITION_HOLDER = '[name]'
+#
+#
+# with open("./Input/Names/invited_names.txt") as invited_name:
+#     list_invited_name = invited_name.readlines()
+#
+# with open("./Input/Letters/starting_letter.txt") as starting_letter:
+#     read_starting_letter = starting_letter.read()
+#     for new_name in list_invited_name:
+#         strip_name = new_name.strip()
+#         create_new_letter_with_mew_name = read_starting_letter.replace(POSITION_HOLDER, strip_name)
+#         with open(f"./Output/ReadyToSend/letter_for_{strip_name}", mode='w') as new_ready_letter:
+#             new_ready_letter = new_ready_letter.write(create_new_letter_with_mew_name)
+#
+#             print(strip_name)
+#
+POSITION = "[name]"
+with open("./Input/Names/invited_names.txt") as name:
+    names = name.readlines()
+
+
+with open("./Input/Letters/starting_letter.txt") as letter:
+    letters = letter.read()
+    for one_person in names:
+        strip_one_person = one_person.strip()
+        new_letters = letters.replace(POSITION, strip_one_person)
+
+        print(new_letters)
